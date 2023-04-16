beacb78519decc296279a07d40db83c7fcacd208
Koen Klinkers <k.klinkers@gmail.com>
2023-04-16 15:57:07 +0200

    add maxlenght for message

--- chat/assistant.py
+++ chat/assistant.py
@@ -53,7 +53,9 @@ def git_prompt():
         if "readme" in file.lower():
             readme = run_bash(f"cat {file}")
             break
-    return f"You're are the assisent of a developer for a specific repository. This is the Readme:{readme}. This is the output of git status: {git_status}, This is the current git diff: {git_diff}"
+    return f"You're are the assisent of a developer for a specific repository. This is the Readme:{readme}. This is the output of git status: {git_status}, This is the current git diff: {git_diff}"[
+        :4000
+    ]
 
 
 ASSISTANTS["git"] = Assistant(
