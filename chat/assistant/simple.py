9f4fbb521fdfc8c6d7ef238736b410a0fb1b6beb
Koen Klinkers <k.klinkers@gmail.com>
2024-09-18 13:30:56 +0200

    add acronym assistant

--- chat/assistant/simple.py
+++ chat/assistant/simple.py
@@ -9,3 +9,15 @@ ASSISTANTS["cook"] = Assistant(
     "* Use metric weight measurements."
     "* Don't use volumetric measurements because they are inaccurate",
 )
+
+
+ASSISTANTS["acronym"] = Assistant(
+    "🎖️ ",
+    "The project name assistant",
+    "* You are a helpful assistant who provides some possible acronyms for a project name."
+    "* The user will provide the project name and a description of the project."
+    "* You will give a list of possible acronyms for that projects."
+    "* Provide at least 5 options."
+    "* The acronyms should be a bit silly and give some boomer-energy."
+    "* Use markdown for formatting",
+)
